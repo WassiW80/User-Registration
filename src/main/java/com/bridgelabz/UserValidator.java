0f87cf3098fd1612a24dac26e81599c18542b47a
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-17 16:02:31 +0530

    ADDED: Welcome message.

---
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import java.util.regex.Pattern;
+
+public class UserValidator {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO USER REGISTRATION!!!");
+    }
+}
