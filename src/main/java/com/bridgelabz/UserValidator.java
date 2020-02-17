ca58497b853e761603953de127ccfb52f0711f06
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-17 18:09:50 +0530

    ADDED: Validating last name.

--- src/main/java/com/bridgelabz/UserValidator.java
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -1,17 +1,15 @@
 package com.bridgelabz;
 
-import java.util.regex.Pattern;
-
 public class UserValidator {
-    private static final String FIRSTNAME = "^[A-Z]{1}[a-z]{2,}";
+    private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
 
-    public static boolean validateFirstName(String fname) {
-        if (fname.matches(FIRSTNAME)) {
-            return true;
-        } else {
-            return false;
-        }
+    public static boolean validateFirstName(String name) {
+        return name.matches(NAME_PATTERN);
     }
+    public static boolean validateLastName(String name) {
+        return name.matches(NAME_PATTERN);
+    }
+
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO USER REGISTRATION!!!");
--- src/test/java/com/bridgelabz/UserValidationTest.java
+++ src/test/java/com/bridgelabz/UserValidationTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserValidationTest {
+
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         boolean result = UserValidator.validateFirstName("Wassi");
@@ -33,4 +34,37 @@ public class UserValidationTest {
         boolean result = UserValidator.validateFirstName("1234");
         Assert.assertFalse(result);
     }
+
+    //Test cases for validating last name
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        boolean result = UserValidator.validateLastName("Sayed");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLastName_WhenAllLowerCase_ShouldReturnFalse() {
+        boolean result = UserValidator.validateLastName("sayed");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLastName_WhenTwoCharacterPresent_ShouldReturnFalse() {
+        boolean result = UserValidator.validateLastName("sa");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLastName_whenAllUpperCase_ShouldReturnFalse() {
+        boolean result = UserValidator.validateLastName("SAYED");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLastName_whenDigitsEntered_ShouldReturnFalse() {
+        boolean result = UserValidator.validateLastName("1234");
+        Assert.assertFalse(result);
+    }
+
+
 }
